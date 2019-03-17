b382aa5d2a35c82180040b3eb81cc387689a4b4b
Harikrishnan <hkkrishn563@gmail.com>
2019-03-16 19:41:39 -0700

    coins spotlight implemented

---
+++ src/Dashboard/CoinSpotlight.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Tile} from '../Shared/Tile';
+import {AppContext} from '../App/AppProvider';
+import CoinImage from '../Shared/CoinImage';
+import styled from 'styled-components';
+
+
+const SpotLighName = styled.h2`
+  text-align:center;
+`
+
+export default function() {
+  return (
+    <AppContext.Consumer>
+      {({currentFavourite,coinList})=>
+        <Tile> 
+         <SpotLighName> {coinList[currentFavourite].CoinName} </SpotLighName> 
+         <CoinImage spotlight coin = {coinList[currentFavourite]}/>
+        </Tile>
+      }
+    </AppContext.Consumer>
+  )
+}
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -7,12 +7,24 @@ import CoinGrid from '../Settings/CoinGrid';
 import Page from '../Shared/Page';
 import Search from '../Settings/Search';
 import PriceGrid from './PriceGrid';
+import CoinSpotlight from './CoinSpotlight';
+import styled from 'styled-components'
 
 
+const ChartGrid = styled.div`
+  display:grid;
+  margin-top:20px;
+  grid-gap:15px;
+  grid-template-columns:1fr 3fr;
+`;
 
 export default function() {
   return <Page name = 'dashboard'>
       <PriceGrid/>
+      <ChartGrid>
+        <CoinSpotlight/>
+        <div>Chart Goes Here</div>
+      </ChartGrid>
     </Page>
   
 }
